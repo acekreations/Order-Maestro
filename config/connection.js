c82e5adeef2341673fb0829f4473009a53da3fa8
Craig Melville <acekreations@gmail.com>
2018-09-13 14:21:38 -0700

    database fix

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-var Sequelize = require("sequelize");
-
-var sequelize = new Sequelize("order_maestro_db", "root", "password", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// var Sequelize = require("sequelize");
+//
+// var sequelize = new Sequelize("order_maestro_db", "root", "password", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+var mysql = require("mysql2");
 
 var connection;
 if (process.env.JAWSDB_URL) {
