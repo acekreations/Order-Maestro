d38067bfcaf03bb1e073c07693d413c8a61de84c
Craig Melville <acekreations@gmail.com>
2018-09-13 00:43:20 -0700

    complete create and outstanding handlebars files

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -6,7 +6,11 @@ var Order = require("../models/orders.js");
 var Plate = require("../models/plates.js");
 
 router.get("/", function(req, res){
-  Plate.findAll({}).then(function(plates){
+  Plate.findAll({
+    order: [
+      ['name', 'ASC'],
+    ],
+  }).then(function(plates){
     res.render("index", {plates: plates, active: { create: true}});
   });
 });
@@ -14,16 +18,39 @@ router.get("/", function(req, res){
 router.get("/outstanding/:id", function(req, res){
   Order.findAll({
     where: {
-      user_id: req.body.id,
+      user_id: req.params.id,
       completed: false
-    }
+    },
+    order: [
+      ['id', 'ASC'],
+    ],
   }).then(function(orders){
+    console.log(JSON.parse(orders[0].dataValues.items));
+    for (var i = 0; i < orders.length; i++) {
+      orders[i].dataValues.items = JSON.parse(orders[i].dataValues.items);
+    }
+
     res.render("outstanding", {orders: orders, active: { outstanding: true}});
   });
 });
 
-router.get("/completed", function(req, res){
-  res.render("completed", {active: { completed: true}});
+router.get("/completed/:id", function(req, res){
+  Order.findAll({
+    where: {
+      user_id: req.params.id,
+      completed: true
+    },
+    order: [
+      ['id', 'DESC'],
+    ],
+  }).then(function(orders){
+    console.log(JSON.parse(orders[0].dataValues.items));
+    for (var i = 0; i < orders.length; i++) {
+      orders[i].dataValues.items = JSON.parse(orders[i].dataValues.items);
+    }
+
+    res.render("completed", {orders: orders, active: { completed: true}});
+  });
 });
 
 module.exports = router;
